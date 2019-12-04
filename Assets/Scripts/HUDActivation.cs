8eedba868822b82b7cb344a1ba9da9d57e0d833d
Jeronimo Grandi <jggrandi@gmail.com>
2019-12-03 17:49:18 -0800

    UI HUD start implementation

---
+++ Assets/Scripts/HUDActivation.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDActivation : Raycaster
+{
+
+    private IEnumerator coroutine;
+
+    public GameObject objTriggered;
+
+    protected override void OnRaycasterEnter(GameObject target)
+    {
+        objTriggered = target;
+        Debug.Log("DANNNMKKHUIHUIO!");
+    }
+
+    //protected override void OnRaycasterLeave(GameObject target)
+    //{
+    //    coroutine = WaitToHideUI(target);
+    //    StartCoroutine(coroutine);
+
+    //}
+
+    //IEnumerator WaitToShowUI(GameObject target)
+    //{
+
+
+    //}
+    //IEnumerator WaitToHideUI(GameObject target)
+    //{
+    //    yield return new WaitForSeconds(.5f);
+    //    Debug.Log("LEAVE!");
+    //    for (int i = 0; i < target.transform.childCount; i++)
+    //        target.transform.GetChild(i).gameObject.SetActive(false);
+
+    //}
+
+
+
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/ToggleArmUIOnLook.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleArmUIOnLook : Raycaster
+{
+
+    private IEnumerator coroutine;
+
+    protected override void OnRaycasterEnter(GameObject target)
+    {
+        coroutine = WaitToShowUI(target);
+        StartCoroutine(coroutine);
+    }
+
+    protected override void OnRaycasterLeave(GameObject target)
+    {
+        coroutine = WaitToHideUI(target);
+        StartCoroutine(coroutine);
+
+    }
+
+    IEnumerator WaitToShowUI(GameObject target)
+    {
+        yield return new WaitForSeconds(.2f);
+        Debug.Log("HIT!");
+        for (int i = 0; i < target.transform.childCount; i++)
+            target.transform.GetChild(i).gameObject.SetActive(true);
+        
+
+    }
+    IEnumerator WaitToHideUI(GameObject target)
+    {
+        yield return new WaitForSeconds(.5f);
+        Debug.Log("LEAVE!");
+        for (int i = 0; i < target.transform.childCount; i++)
+            target.transform.GetChild(i).gameObject.SetActive(false);
+
+    }
+
+
+
+
+
+}
