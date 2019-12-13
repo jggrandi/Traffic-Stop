666091eeacc95235d8297ffa782240c25354f950
Jeronimo Grandi <jggrandi@gmail.com>
2019-12-13 14:55:07 -0800

    Add instruction text

---
+++ Assets/DisableText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AlertsHandler.TriggerResultOfPlateScan += DisableInstructions;
+    }
+
+    void DisableInstructions()
+    {
+        gameObject.SetActive(false);
+    }
+}
