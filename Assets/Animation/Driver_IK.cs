947a68c233128adb729bc20767628e30d43c23fd
Zachery <zc68@duke.edu>
2020-01-27 09:38:34 -0500

    Add Final IK plugin.

---
+++ Assets/Animation/Driver_IK.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RootMotion.FinalIK;
+
+
+public class Driver_IK : MonoBehaviour
+{
+    public FullBodyBipedIK ik;
+    public Transform leftHandTarget, rightHandTarget, leftFootTarget, rightFootTarget;
+    public bool rightHandLock = true;
+
+    private void LateUpdate()
+    {
+        if (rightHandLock)
+        {
+            ik.solver.rightHandEffector.position = rightHandTarget.position;
+            ik.solver.rightHandEffector.rotation = rightHandTarget.rotation;
+            ik.solver.rightHandEffector.positionWeight = 1f;
+            ik.solver.rightHandEffector.rotationWeight = 1f;
+        }
+        else
+        {
+            ik.solver.rightHandEffector.positionWeight = 0f;
+            ik.solver.rightHandEffector.rotationWeight = 0f;
+        }
+        ik.solver.leftHandEffector.position = leftHandTarget.position;
+        ik.solver.leftHandEffector.rotation = leftHandTarget.rotation;
+        ik.solver.leftHandEffector.positionWeight = 1f;
+        ik.solver.leftHandEffector.rotationWeight = 1f;
+        ik.solver.rightFootEffector.position = rightFootTarget.position;
+        ik.solver.rightFootEffector.rotation = rightFootTarget.rotation;
+        ik.solver.rightFootEffector.positionWeight = 1f;
+        ik.solver.rightFootEffector.rotationWeight = 1f;
+        ik.solver.leftFootEffector.position = leftFootTarget.position;
+        ik.solver.leftFootEffector.rotation = leftFootTarget.rotation;
+        ik.solver.leftFootEffector.positionWeight = 1f;
+        ik.solver.leftFootEffector.rotationWeight = 1f;
+    }
+}
